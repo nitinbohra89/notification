024537e8de529d107a4bed6a54675bcf1fea643e
bohra <bohra@Bohra-Lappy>
2017-09-18 08:55:08 +0530

    a

--- src/main/java/com/automobile/notification/utility/DBUtility.java
+++ src/main/java/com/automobile/notification/utility/DBUtility.java
@@ -18,11 +18,12 @@ public class DBUtility {
 
 		try {
 			String jdbcUrl = null;
+			Class.forName("com.mysql.jdbc.GoogleDriver");
 			if (useGoogleConnector) {
 				Class.forName("com.mysql.jdbc.GoogleDriver");
 				jdbcUrl = String.format(cloudUrl, database, instance, socketFactory);
 			} else {
-				Class.forName("com.mysql.cj.jdbc.Driver");
+			//	Class.forName("com.mysql.cj.jdbc.Driver");
 				jdbcUrl = String.format(localUrl, database, instance, socketFactory);
 			}
 			System.out.println("Class Loaded");
